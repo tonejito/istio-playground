c1e4f4749fbb5f6a1dc68a5bb80c0838014526c8
Andrés Hernández <andres@tonejito.org>
2022-01-18 13:00:00 -0600

    Pin istio version (kvm)

--- minikube-kvm-istio-helm.sh
+++ minikube-kvm-istio-helm.sh
@@ -2,10 +2,17 @@
 
 MINIKUBE_PROFILE=minikube-kvm
 
+TIMEOUT=5m
+
+################################################################################
+# Cleanup
+
 reset
 minikube delete --profile ${MINIKUBE_PROFILE}
-
 sleep 10
+
+################################################################################
+# Create minikube instance
 
 minikube start --driver=kvm2 --cpus=4 --memory=8g --profile ${MINIKUBE_PROFILE}
 minikube profile list
@@ -13,29 +20,54 @@ kubectl config get-contexts
 
 sleep 30
 
-kubectl wait --for condition=ready pods -l k8s-app=kube-proxy -n kube-system --timeout=5m
-kubectl wait --for condition=ready pods -l k8s-app=kube-dns   -n kube-system --timeout=5m
+########################################
+# Wait for cluster to be ready
+
+NAMESPACE=kube-system
+for APP in kube-proxy kube-dns
+do
+  kubectl wait --for condition=ready pod -l k8s-app=${APP} -n ${NAMESPACE} --timeout=${TIMEOUT}
+done
 
 sleep 5
 
-minikube addons enable dashboard --profile ${MINIKUBE_PROFILE}
-kubectl wait --for condition=ready pod -l k8s-app=kubernetes-dashboard      -n kubernetes-dashboard --timeout=5m
-kubectl wait --for condition=ready pod -l k8s-app=dashboard-metrics-scraper -n kubernetes-dashboard --timeout=5m
+########################################
+# Enable kubernetes-dashboard addon
 
-# minikube addons enable metrics-server --profile ${MINIKUBE_PROFILE}
+minikube addons enable dashboard --profile ${MINIKUBE_PROFILE}
+NAMESPACE=kubernetes-dashboard
+for APP in kubernetes-dashboard dashboard-metrics-scraper
+do
+  kubectl wait --for condition=ready pod -l k8s-app=${APP} -n ${NAMESPACE} --timeout=${TIMEOUT}
+done
+
+########################################
+# Enable metrics-server addon
+
+minikube addons enable metrics-server --profile ${MINIKUBE_PROFILE}
+NAMESPACE=kube-system
+for APP in metrics-server
+do
+  kubectl wait --for condition=ready pod -l k8s-app=${APP} -n ${NAMESPACE} --timeout=${TIMEOUT}
+done
 
 kubectl get all --all-namespaces
 
+################################################################################
+# Install and configure MetalLB
+
 METALLB_IP_START=192.168.39.100
-METALLB_IP_END=192.168.39.200
+METALLB_IP_END=192.168.39.110
 minikube addons list --profile ${MINIKUBE_PROFILE}
 minikube addons enable metallb --profile ${MINIKUBE_PROFILE}
 sleep 10
-echo -e "${METALLB_IP_START}\n${METALLB_IP_END}\n" | minikube addons configure metallb --profile ${MINIKUBE_PROFILE} || true
+echo -e "${METALLB_IP_START}\n${METALLB_IP_END}\n" | \
+minikube addons configure metallb --profile ${MINIKUBE_PROFILE} || true
 sleep 10
 kubectl get configmap/config -n metallb-system
 
-cat > metallb-config.yaml << EOF
+# Patch MetalLB config with updated IP address range
+kubectl apply -f - -n metallb-system << EOF
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -50,14 +82,21 @@ data:
       - ${METALLB_IP_START}-${METALLB_IP_END}
 EOF
 
-kubectl apply -n metallb-system -f metallb-config.yaml
-
-kubectl wait --for condition=ready pod -l app=metallb -l component=controller -n metallb-system --timeout=5m
-kubectl wait --for condition=ready pod -l app=metallb -l component=speaker -n metallb-system --timeout=5m
+NAMESPACE=metallb-system
+APP=metallb
+for COMPONENT in controller speaker
+do
+  kubectl wait --for condition=ready pod -l app=${APP} -l component=${COMPONENT} -n ${NAMESPACE} --timeout=${TIMEOUT}
+done
 
 sleep 10
 
 kubectl get all --all-namespaces
+
+################################################################################
+# Install and configure istio with helm
+
+export ISTIO_VERSION=1.12.1
 
 which helm
 helm version
@@ -67,14 +106,14 @@ helm repo update
 sleep 5
 
 kubectl create namespace istio-system
-helm install istio-base istio/base -n istio-system
-helm install istiod istio/istiod -n istio-system --wait
+helm install istio-base istio/base -n istio-system --version ${ISTIO_VERSION} --wait --timeout=${TIMEOUT}
+helm install istiod istio/istiod -n istio-system --version ${ISTIO_VERSION} --wait --timeout=${TIMEOUT}
 
 sleep 15
 
 kubectl create namespace istio-ingress
 kubectl label namespace istio-ingress istio-injection=enabled --overwrite
-helm install istio-ingress istio/gateway -n istio-ingress --wait
+helm install istio-ingress istio/gateway -n istio-ingress  --version ${ISTIO_VERSION} --wait --timeout=${TIMEOUT}
 
 sleep 15
 
